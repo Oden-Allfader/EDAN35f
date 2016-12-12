1fc808c9cda27a0a343f991429c88e4162277833
Oden-Allfader <typdude@hotmail.com>
2016-12-12 18:07:24 +0100

    Update assignment5.cpp

--- assignment5.cpp
+++ assignment5.cpp
@@ -28,6 +28,8 @@
 #include <map>
 #include <stddef.h>
 
+
+
 enum class polygon_mode_t : unsigned int {
 	fill = 0u,
 	line,
@@ -43,11 +45,11 @@ static polygon_mode_t get_next_mode(polygon_mode_t mode)
 	return static_cast<polygon_mode_t>((static_cast<unsigned int>(mode) + 1u) % 3u);
 }
 static glm::vec3 startPosition() {
-	float x = (rand()%12-6)/3;
+	float x = (rand() % 12 - 6) / 3;
 	float z = (rand() % 12 - 6) / 3;
 	float h = 4;
 	float y = (rand() % 12 - 6) / 3;
-	return glm::vec3(x, y,z);
+	return glm::vec3(x, y, z);
 }
 bool testSphereSphere(glm::vec3 const&p1, float const r1, glm::vec3 const&p2, float const r2) {
 	return glm::length(p1 - p2) < (r1 + r2);
@@ -80,10 +82,14 @@ eda221::Assignment5::~Assignment5()
 }
 void eda221::Assignment5::run()
 {
+	const int MaxFires = 10;
+	const int MaxSmokes = 30;
+
+
 	// Loading the level geometry
-	auto const quad_shape = parametric_shapes::createQuad(4, 4);
+	auto const fire_shape = parametric_shapes::createQuad(6, 6);
 
-
+	auto const smoke_shape = parametric_shapes::createQuad(2,1);
 
 	// Set up the camera 
 	// Get the camera to follow the snake.
@@ -99,35 +105,47 @@ void eda221::Assignment5::run()
 
 	// Create the shader programs
 	// Water shader for the level
-	auto diffuse_shader = eda221::createProgram("diffuse.vert", "diffuse.frag");
-	if (diffuse_shader == 0u) {
-		LogError("Failed to load water shader");
+	auto fire_shader = eda221::createProgram("fire.vert", "fire.frag");
+	if (fire_shader == 0u) {
+		LogError("Failed to load fire shader");
 		return;
 	}
-	auto const reload_shaders = [&diffuse_shader]() {
-		if (diffuse_shader != 0u)
-			glDeleteProgram(diffuse_shader);
-		diffuse_shader = eda221::createProgram("diffuse.vert", "diffuse.frag");
-		if (diffuse_shader == 0u)
+	auto smoke_shader = eda221::createProgram("smoke.vert", "smoke.frag");
+	if (smoke_shader == 0u) {
+		LogError("Failed to load smoke shader");
+		return;
+	}
+
+
+
+
+
+	auto const reload_shaders = [&fire_shader]() {
+		if (fire_shader != 0u)
+			glDeleteProgram(fire_shader);
+		fire_shader = eda221::createProgram("fire.vert", "fire.frag");
+		if (fire_shader == 0u)
 			LogError("Failed to load diffuse shader");
 	};
 	reload_shaders();
+
+
 
 	// Initializing the time variables.
 	f64 ddeltatime;
 	size_t fpsSamples = 0;
 	double nowTime, lastTime = GetTimeMilliseconds();
 	double fpsNextTick = lastTime + 1000.0;
-
+	float number = 0;
 
 	// Set the uniforms of the shaders
 	auto light_position = glm::vec3(-90.0f, 100.0f, -90.0f); // Light position;
 	auto camera_position = mCamera.mWorld.GetTranslation();
-	auto const set_uniforms = [&light_position, &camera_position, &nowTime](GLuint program) {
+	auto const set_uniforms = [&light_position, &camera_position, &nowTime, &number](GLuint program) {
 		glUniform3fv(glGetUniformLocation(program, "light_position"), 1, glm::value_ptr(light_position));
 		glUniform3fv(glGetUniformLocation(program, "camera_position"), 1, glm::value_ptr(camera_position));
 		glUniform1f(glGetUniformLocation(program, "time"), static_cast<float>(nowTime) / 1000.0f);
-
+		glUniform1f(glGetUniformLocation(program, "number"),number);
 
 
 	};
@@ -136,59 +154,81 @@ void eda221::Assignment5::run()
 
 	auto fireTex = loadTexture2D("flame.png");
 
-	
+
 
 	//Fire Node
-	const int MaxFires = 16;
 	glm::vec3 fires[MaxFires];
 	Node quad[MaxFires];
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			quad[4*i + j ].set_geometry(quad_shape);
-			quad[4 * i + j].add_texture("diffuse_texture", fireTex);
-			quad[4 * i + j].set_program(diffuse_shader, set_uniforms);
-			//quad[i].scale(glm::vec3(0.1, 0.1, 0.1));
-			//quad[i].rotate_x(bonobo::pi / 2);
 
-			/*
-			
-			float cos_phi = std::cos(phi),
-				sin_phi = std::sin(phi);
-			vertices[index] = glm::vec3(radius * sin_theta*sin_phi,
-				-radius*cos(phi),
-				radius*cos_theta*sin_phi);
-			*/
+	//Initializing positions.
+	for (int i = 0; i < MaxFires; i++) {
+		quad[i ].set_geometry(fire_shape);
+		quad[i].add_texture("diffuse_texture", fireTex);
+		quad[ i].set_program(fire_shader, set_uniforms);
+	}
+	fires[0] = glm::vec3(-2,0,0);
+	quad[0].set_translation(fires[0]);
+	fires[1] = glm::vec3(-1.5, 0, -1.5);
+	quad[1].set_translation(fires[1]);
+	fires[2] = glm::vec3(1.5, 0, -1.5);
+	quad[2].set_translation(fires[2]);
+	fires[3] = glm::vec3(2, 0, 0);
+	quad[3].set_translation(fires[3]);
+	fires[4] = glm::vec3(1.5, 0, 1.5);
+	quad[4].set_translation(fires[4]);
+	fires[5] = glm::vec3(-1.5, 0,1.5);
+	quad[5].set_translation(fires[5]);
+	fires[6] = glm::vec3(-1, 0.5, -1);
+	quad[6].set_translation(fires[6]);
+	fires[7] = glm::vec3(1, 0.5, -1);
+	quad[7].set_translation(fires[7]);
+	fires[8] = glm::vec3(0, 0.5, 1);
+	quad[8].set_translation(fires[8]);
+	fires[9] = glm::vec3(0, 1, 0);
+	quad[9].set_translation(fires[9]);
 
-			float r = 2;
-			float phi = (static_cast<float>(j)/4)*bonobo::two_pi;
-			float theta = (static_cast<float>(i)/4)* bonobo::pi;
-			float x_pos = r*glm::sin(theta)*glm::cos(phi);
-			float y_pos = r*glm::sin(theta)*glm::sin(phi);
-			float z_pos = r*cos(theta);
-
-
-			//float y_pos = 0;
-
-			fires[4 * i + j] = glm::vec3(x_pos, y_pos, z_pos);
-
-
-
-
-
-			quad[4 * i + j].set_translation(fires[4 * i + j]);
-		}
-
+	////SMOKE MY LIFE AWAY
+	auto smokeTex = loadTexture2D("smoke.png");
+	glm::vec3 smokes[MaxSmokes];
+	Node smokeQuad[MaxSmokes];
+	for (int i = 0; i < MaxSmokes; i++) {
+		smokeQuad[i].set_geometry(smoke_shape);
+		smokeQuad[i].add_texture("diffuse_texture",smokeTex);
+		smokeQuad[i].set_program(smoke_shader,set_uniforms);
+		smokes[i] = glm::vec3(0.0,i/2,0.0);
+		smokeQuad[i].set_translation(smokes[i]);
 	}
 
+
+	
+	////SMOKE ENDS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	glEnable(GL_DEPTH_TEST);
+	glDisable(GL_DEPTH_TEST);
 	// Enable face culling to improve performance:
 	//glEnable(GL_CULL_FACE);
 	//glCullFace(GL_FRONT);
 	//glCullFace(GL_BACK);
 
-	//Initializing positions.
+
 	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA,GL_ONE);
+	glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+
 	while (!glfwWindowShouldClose(window->GetGLFW_Window())) {
 		nowTime = GetTimeMilliseconds();
 		ddeltatime = nowTime - lastTime;
@@ -227,90 +267,84 @@ void eda221::Assignment5::run()
 		}
 
 		camera_position = mCamera.mWorld.GetTranslation();
-
-
-
 		auto const window_size = window->GetDimensions();
 		glViewport(0, 0, window_size.x, window_size.y);
 		glClearDepthf(1.0f);
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
-		//sort here
-		std::map<float, glm::vec3> sorted;
-		for (GLuint i = 0; i < MaxFires; i++) // windows contains all window positions
-		{
-			GLfloat distance = glm::length(camera_position - fires[i]);
-			sorted[distance] = fires[i];
+		for (int i = 0; i < MaxSmokes/3; i++) {
+			smokes[i].x = sin((1000 * static_cast<float>(i) + nowTime) / 1500) + pow(sin(nowTime / 1900),2);
+			smokes[i].y = fmod((1000 * static_cast<float>(i) + nowTime) / 1000, 10);
+			smokes[i].z = sin((1000 * static_cast<float>(i) + nowTime) / 2000) + pow(sin(nowTime / 1000), 2);
 		}
-
-
+		for (int i = MaxSmokes/3; i < 2*MaxSmokes/3; i++) {
+			smokes[i].x = -1.0 - sin((1000 * static_cast<float>(i) + nowTime) / 1750) + pow(sin(nowTime / 1700), 2);
+			smokes[i].y = 1.0+fmod((1000 * static_cast<float>(i) + nowTime) / 1000, 10);
+			smokes[i].z = sin((1000 * static_cast<float>(i) + nowTime) / 1337) + pow(sin(nowTime / 1200), 3);
+		}
+		for (int i = 2*MaxSmokes / 3; i < MaxSmokes ; i++) {
+			smokes[i].x = -1.5 + sin((1000 * static_cast<float>(i) + nowTime) / 1337) + pow(sin(nowTime / 1200), 3);
+			smokes[i].y = 2.0 + fmod((1000 * static_cast<float>(i) + nowTime) / 1000, 10);
+			smokes[i].z = -1-sin((1000 * static_cast<float>(i) + nowTime) / 1337) + pow(sin(nowTime / 2000), 5);
+		}
+		//sort here
+		//std::map<float, glm::vec3> sorted;
+		//for (GLuint i = 0; i < MaxFires; i++) // windows contains all window positions
+		//{
+		//	GLfloat distance = glm::distance(camera_position, fires[i]);
+		//	sorted[distance] = fires[i];
+		//}
+		
+		//std::map<float, glm::vec3> sortedSmoke;
+		//for (GLuint i = 0; i < MaxSmokes; i++) // windows contains all window positions
+		//{
+		//	GLfloat distance = glm::distance(camera_position, smokes[i]);
+		//	sortedSmoke[distance] = smokes[i];
+		//}
 
 
 		//render here
-		int i = 0;
-		for (std::map<float, glm::vec3>::reverse_iterator it = sorted.rbegin(); it != sorted.rend(); ++it)
-		{
-
-
-			glm::vec3 q_position = it->second;
-			quad[i].set_translation(q_position);
-
-			//glm::vec3 v1 = glm::normalize(camera_position - q_position);
-			//auto normal_model_to_world = glm::transpose(glm::inverse(quad[i].get_transform()));
-			//for (int k = 0; k < 4; k++) {
-			//	for (int m = 0; m < 4; m++) {
-			//		std::printf("%f ",normal_model_to_world[k][m]);
-			//	}
-			//	std::printf("\n");
-			//}
-
-			//glm::vec3 v1x = glm::vec3(0.0,v1.y,v1.z);
-			//glm::vec3 v1y = glm::vec3(v1.x, 0.0, v1.z);
-			//glm::vec3 v1z = glm::vec3(v1.x, v1.y, 0.0);
-
-
-
-			//glm::vec3 v2x = glm::vec3(normal_model_to_world *  glm::vec4(1.0, 0.0, 0.0, 0.0));
-			//glm::vec3 v2y = glm::vec3(normal_model_to_world *  glm::vec4(0.0, 1.0, 0.0, 0.0));
-			//glm::vec3 v2z = glm::vec3(normal_model_to_world *  glm::vec4(0.0, 0.0, 1.0, 0.0));
-			//float dotx = glm::dot(glm::vec2(v1.y, v1.z), glm::vec2(v2x.y, v2x.z));
-			//float dotx = glm::dot(v1x,(v2z+v2y));
-			//float angle_x = glm::acos(dotx);
-
-			//float doty = glm::dot(v1y, (v2z + v2x));
-			//float angle_y = glm::acos(doty);
-
-			//float dotz = glm::dot(v1z, (v2y+v2x));
-			//float angle_z = glm::acos(dotz);
-
-			//std::printf("v1: %f %f %f \t v2: %f %f %f \t",v1x.x,v1x.y,v1x.z,v2x.x,v2x.y,v2x.z);
-
-			//std:printf("angle: %f \n",angle_x);
-			//float doty = glm::dot(glm::vec2(v1.x, v1.z), glm::vec2(v2y.x, v2y.z));
-			//float angle_y = glm::acos(doty / (glm::length(glm::vec2(v1.x, v1.z))*glm::length(glm::vec2(v2y.x, v2y.z))));
-
-			//float dotz = glm::dot(glm::vec2(v1.x, v1.y), glm::vec2(v2z.x, v2z.y));
-			//float angle_z = glm::acos(dotz / (glm::length(glm::vec2(v1.x, v1.y))*glm::length(glm::vec2(v2z.x, v2z.y))));
-
-			//quad[i].set_rotation_x(angle_x);
-			//quad[i].set_rotation_y(angle_y);
-			//quad[i].set_rotation_z(angle_z);
-
-			//quad[i].rotate_z(bonobo::pi/144);
-			//quad[i].rotate_y(glm::sin(nowTime / 1000));
-			//quad[i].rotate_z(glm::sin(nowTime / 1000));
-
-			//glm::vec3 up = glm::vec3(normal_model_to_world * glm::vec4(1.0, 0.0, 0.0, 0.0));
-			//glm::mat4 rotate = glm::lookAt(q_position, camera_position, up);
-
-			//
-
-
-
-			quad[i].render(mCamera.GetWorldToClipMatrix(), quad[i].get_transform(),mCamera.GetWorldToViewMatrix(),mCamera.GetViewToClipMatrix(), q_position);
-			i++;
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		for (int i= 0; i < MaxFires; i++) {
+			glm::vec3 q_position = glm::vec3(0, 0, 0);
+			quad[i].render(mCamera.GetWorldToClipMatrix(), quad[i].get_transform(), mCamera.GetWorldToViewMatrix(), mCamera.GetViewToClipMatrix(), q_position);
 		}
+
+		for (int i = 0; i < MaxSmokes; i++) {
+			smokeQuad[i].set_translation(smokes[i]);
+			number = fmod((1000 * static_cast<float>(i) + nowTime) / 1000, 10);
+			glm::vec3 q_position = glm::vec3(0, 0, 0);
+			smokeQuad[i].render(mCamera.GetWorldToClipMatrix(), smokeQuad[i].get_transform(), mCamera.GetWorldToViewMatrix(), mCamera.GetViewToClipMatrix(), q_position);
+		}
+
+		//int i = 0;
+		//for (std::map<float, glm::vec3>::reverse_iterator it = sorted.rbegin(); it != sorted.rend(); ++it)
+		//{
+
+
+		//	glm::vec3 q_position = it->second;
+		//	quad[i].set_translation(q_position);
+
+
+		//	quad[i].render(mCamera.GetWorldToClipMatrix(), quad[i].get_transform(), mCamera.GetWorldToViewMatrix(), mCamera.GetViewToClipMatrix(), q_position);
+		//	i++;
+		//}
+		//int i = 0;
+
+		//for (std::map<float, glm::vec3>::reverse_iterator it = sortedSmoke.rbegin(); it != sortedSmoke.rend(); ++it)
+		//{
+
+
+		//	glm::vec3 q_position = it->second;
+		//	smokeQuad[i].set_translation(q_position);
+		//	
+		//	number = fmod((1000*static_cast<float>(i) + nowTime) / 1000, 10);
+		//	smokeQuad[i].render(mCamera.GetWorldToClipMatrix(), smokeQuad[i].get_transform(), mCamera.GetWorldToViewMatrix(), mCamera.GetViewToClipMatrix(), q_position);
+		//	i++;
+		//}
+
+
 
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
@@ -323,7 +357,7 @@ void eda221::Assignment5::run()
 
 		ImGui::Begin("Render Time", &opened, ImVec2(120, 50), -1.0f, 0);
 		if (opened)
-			ImGui::Text(" %.0f fps,  Camera Postion: %f, %f, %f \t Quad1 Position: %f, %f, %f", 1000 / (ddeltatime), camera_position.x, camera_position.y, camera_position.z, fires[90].x, fires[90].y, fires[90].z);
+			ImGui::Text(" %.0f fps,  Camera Postion: %f, %f, %f \t Quad1 Position: %f, %f, %f", 1000 / (ddeltatime), camera_position.x, camera_position.y, camera_position.z, fires[0].x, fires[0].y, fires[0].z);
 		ImGui::End();
 
 		ImGui::Render();
@@ -331,8 +365,10 @@ void eda221::Assignment5::run()
 		lastTime = nowTime;
 
 	}
-	glDeleteProgram(diffuse_shader);
-	diffuse_shader = 0u;
+	glDeleteProgram(fire_shader);
+	fire_shader = 0u;
+	glDeleteProgram(smoke_shader);
+	smoke_shader = 0u;
 }
 
 int main()
