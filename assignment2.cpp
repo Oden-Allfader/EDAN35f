257451c1f2b5535dec53408f10fc68e07593dee0
Oden-Allfader <typdude@hotmail.com>
2016-12-13 22:08:00 +0100

    Add files via upload

--- assignment2.cpp
+++ assignment2.cpp
@@ -1,6 +1,7 @@
 #include "assignment2.hpp"
 #include "helpers.hpp"
 #include "node.hpp"
+#include "parametric_shapes.hpp"
 
 #include "config.hpp"
 #include "external/glad/glad.h"
@@ -46,10 +47,13 @@ namespace constant
 	constexpr uint32_t shadowmap_res_x = 4096;
 	constexpr uint32_t shadowmap_res_y = 4096;
 
-	constexpr size_t lights_nb           = 4;
+	constexpr size_t lights_nb           = 6;
 	constexpr float  light_intensity     = 720000.0f;
 	constexpr float  light_angle_falloff = 0.8f;
 	constexpr float  light_cutoff        = 0.05f;
+
+	constexpr int MaxFires = 10;
+	constexpr int MaxSmokes = 30;
 }
 
 static eda221::mesh_data loadCone();
@@ -92,6 +96,9 @@ edan35::Assignment2::~Assignment2()
 void
 edan35::Assignment2::run()
 {
+
+
+
 	// Load the geometry of Sponza
 	auto const sponza_geometry = eda221::loadObjects("../crysponza/sponza.obj");
 	if (sponza_geometry.empty()) {
@@ -109,6 +116,11 @@ edan35::Assignment2::run()
 	auto const cone_geometry = loadCone();
 	Node cone;
 	cone.set_geometry(cone_geometry);
+
+	auto const fire_shape = parametric_shapes::createQuad(50, 50);
+	auto const smoke_shape = parametric_shapes::createQuad(20, 20);
+
+
 
 	auto const window_size = window->GetDimensions();
 
@@ -132,6 +144,16 @@ edan35::Assignment2::run()
 		LogError("Failed to load fallback shader");
 		return;
 	}
+	auto fire_shader = eda221::createProgram("fire.vert", "fire.frag");
+	if (fire_shader == 0u) {
+		LogError("Failed to load fire shader");
+		return;
+	}
+	auto smoke_shader = eda221::createProgram("smoke.vert", "smoke.frag");
+	if (smoke_shader == 0u) {
+		LogError("Failed to load smoke shader");
+		return;
+	}
 	auto const reload_shader = [fallback_shader](std::string const& vertex_path, std::string const& fragment_path, GLuint& program){
 		if (program != 0u && program != fallback_shader)
 			glDeleteProgram(program);
@@ -142,17 +164,33 @@ edan35::Assignment2::run()
 		}
 	};
 	GLuint fill_gbuffer_shader = 0u, fill_shadowmap_shader = 0u, accumulate_lights_shader = 0u, resolve_deferred_shader = 0u;
-	auto const reload_shaders = [&reload_shader,&fill_gbuffer_shader,&fill_shadowmap_shader,&accumulate_lights_shader,&resolve_deferred_shader](){
+	auto const reload_shaders = [&reload_shader,&fill_gbuffer_shader,&fill_shadowmap_shader,&accumulate_lights_shader,&resolve_deferred_shader,&smoke_shader,&fire_shader](){
 		LogInfo("Reloading shaders");
 		reload_shader("fill_gbuffer.vert",      "fill_gbuffer.frag",      fill_gbuffer_shader);
 		reload_shader("fill_shadowmap.vert",    "fill_shadowmap.frag",    fill_shadowmap_shader);
 		reload_shader("accumulate_lights.vert", "accumulate_lights.frag", accumulate_lights_shader);
 		reload_shader("resolve_deferred.vert",  "resolve_deferred.frag",  resolve_deferred_shader);
+		reload_shader("smoke.vert", "smoke.frag", smoke_shader);
+		reload_shader("fire.vert", "fire.frag", fire_shader);
 	};
 	reload_shaders();
-
+	double ddeltatime;
+	size_t fpsSamples = 0;
+	double nowTime, lastTime = GetTimeMilliseconds();
+	double fpsNextTick = lastTime + 1000.0;
+	float number = 0;
 	auto const set_uniforms = [](GLuint /*program*/){};
-
+	auto camera_position = mCamera.mWorld.GetTranslation();
+	auto const set_uniforms_p = [&mCamera,&camera_position, &nowTime, &number, &window_size](GLuint program) {
+		glUniform3fv(glGetUniformLocation(program, "camera_position"), 1, glm::value_ptr(camera_position));
+		glUniform1f(glGetUniformLocation(program, "time"), static_cast<float>(nowTime) / 1000.0f);
+		glUniform1f(glGetUniformLocation(program, "number"), number);
+		glUniform2f(glGetUniformLocation(program, "inv_res"),
+			1.0f / static_cast<float>(window_size.x),
+			1.0f / static_cast<float>(window_size.y));
+		glUniformMatrix4fv(glGetUniformLocation(program, "view_projection_inverse"), 1, GL_FALSE,
+			glm::value_ptr(mCamera.GetClipToWorldMatrix()));
+	};
 
 	//
 	// Setup textures
@@ -164,8 +202,8 @@ edan35::Assignment2::run()
 	auto const light_specular_contribution_texture = eda221::createTexture(window_size.x, window_size.y);
 	auto const depth_texture                       = eda221::createTexture(window_size.x, window_size.y, GL_TEXTURE_2D, GL_DEPTH_COMPONENT32F, GL_DEPTH_COMPONENT, GL_FLOAT);
 	auto const shadowmap_texture                   = eda221::createTexture(constant::shadowmap_res_x, constant::shadowmap_res_y, GL_TEXTURE_2D, GL_DEPTH_COMPONENT32F, GL_DEPTH_COMPONENT, GL_FLOAT);
-
-
+	auto fireTex = eda221::loadTexture2D("flame.png");
+	auto smokeTex = eda221::loadTexture2D("smoke.png");
 	//
 	// Setup FBOs
 	//
@@ -215,10 +253,10 @@ edan35::Assignment2::run()
 	std::array<glm::vec3, constant::lights_nb> lightColors;
 
 	for (size_t i = 0; i < constant::lights_nb; ++i) {
-		lightTransforms[i].SetTranslate(glm::vec3(0.0f, 125.0f, 0.0f));
-		lightColors[i] = glm::vec3(0.5f + 0.5f * (static_cast<float>(rand()) / static_cast<float>(RAND_MAX)),
-		                           0.5f + 0.5f * (static_cast<float>(rand()) / static_cast<float>(RAND_MAX)),
-		                           0.5f + 0.5f * (static_cast<float>(rand()) / static_cast<float>(RAND_MAX)));
+		lightTransforms[i].SetTranslate(glm::vec3(0.0f, 10.0f, 0.0f));
+		lightColors[i] = glm::vec3(0.886,
+		                           0.345,
+		                           0.133);
 	}
 
 	TRSTransform<f32, glm::defaultp> coneScaleTransform = TRSTransform<f32, glm::defaultp>();
@@ -238,10 +276,52 @@ edan35::Assignment2::run()
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
 
-	double ddeltatime;
-	size_t fpsSamples = 0;
-	double nowTime, lastTime = GetTimeMilliseconds();
-	double fpsNextTick = lastTime + 1000.0;
+	//Fire Node
+	glm::vec3 fires[constant::MaxFires];
+	Node quad[constant::MaxFires];
+
+	//Initializing positions.
+	for (int i = 0; i < constant::MaxFires; i++) {
+		quad[i].set_geometry(fire_shape);
+		quad[i].add_texture("diffuse_texture", fireTex);
+		quad[i].set_program(fire_shader, set_uniforms_p);
+	}
+	float x_offset = 0.0;//1125.0;
+	float y_offset = 10.0;//120.0;
+	float z_offset = 0.0;//400.0;
+	fires[0] = glm::vec3(-20+x_offset, 0+ y_offset, 0+z_offset);
+	quad[0].set_translation(fires[0]);
+	fires[1] = glm::vec3(-15 + x_offset, 0+ y_offset, -15+z_offset);
+	quad[1].set_translation(fires[1]);
+	fires[2] = glm::vec3(15 + x_offset, 0+ y_offset, -15 + z_offset);
+	quad[2].set_translation(fires[2]);
+	fires[3] = glm::vec3(20 + x_offset, 0+y_offset, 0 + z_offset);
+	quad[3].set_translation(fires[3]);
+	fires[4] = glm::vec3(15 + x_offset, 0+y_offset, 15 + z_offset);
+	quad[4].set_translation(fires[4]);
+	fires[5] = glm::vec3(-15 + x_offset, 0+y_offset, 15 + z_offset);
+	quad[5].set_translation(fires[5]);
+	fires[6] = glm::vec3(-10 + x_offset, 5 + y_offset, -10 + z_offset);
+	quad[6].set_translation(fires[6]);
+	fires[7] = glm::vec3(10 + x_offset, 5 + y_offset, -10 + z_offset);
+	quad[7].set_translation(fires[7]);
+	fires[8] = glm::vec3(0 + x_offset, 5 + y_offset, 10 + z_offset);
+	quad[8].set_translation(fires[8]);
+	fires[9] = glm::vec3(0 + x_offset, 10 + y_offset, 0 + z_offset);
+	quad[9].set_translation(fires[9]);
+
+	////SMOKE MY LIFE AWAY
+	glm::vec3 smokes[constant::MaxSmokes];
+	Node smokeQuad[constant::MaxSmokes];
+	for (int i = 0; i < constant::MaxSmokes; i++) {
+		smokeQuad[i].set_geometry(smoke_shape);
+		smokeQuad[i].add_texture("diffuse_texture", smokeTex);
+		smokeQuad[i].set_program(smoke_shader, set_uniforms_p);
+		smokes[i] = glm::vec3(0.0 + x_offset, i / 2 + y_offset, 0.0 + z_offset);
+		smokeQuad[i].set_translation(smokes[i]);
+	}
+
+
 
 	while (!glfwWindowShouldClose(window->GetGLFW_Window())) {
 		nowTime = GetTimeMilliseconds();
@@ -302,7 +382,7 @@ edan35::Assignment2::run()
 		glClear( GL_COLOR_BUFFER_BIT);
 		for (size_t i = 0; i < constant::lights_nb; ++i) {
 			auto& lightTransform = lightTransforms[i];
-			lightTransform.SetRotate(seconds_nb * 0.1f + i * 1.57f, glm::vec3(0.0f, 1.0f, 0.0f));
+			lightTransform.SetRotate(1 * 0.1f + i * bonobo::pi/3, glm::vec3(0.0f, 1.0f, 0.0f));
 
 			auto light_matrix = lightProjection * lightOffsetTransform.GetMatrixInverse() * lightTransform.GetMatrixInverse();
 
@@ -336,7 +416,7 @@ edan35::Assignment2::run()
 			//glClearDepthf(1.0f);
 			//glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 			//glClear(GL_COLOR_BUFFER_BIT);
-			auto const spotlight_set_uniforms = [&window_size,&mCamera,&light_matrix,&lightColors,&lightTransform,&i](GLuint program){
+			auto const spotlight_set_uniforms = [&window_size,&mCamera,&light_matrix,&lightColors,&lightTransform,&i,&nowTime](GLuint program){
 				glUniform2f(glGetUniformLocation(program, "inv_res"),
 				            1.0f / static_cast<float>(window_size.x),
 				            1.0f / static_cast<float>(window_size.y));
@@ -346,6 +426,7 @@ edan35::Assignment2::run()
 				                   glm::value_ptr(mCamera.mWorld.GetTranslation()));
 				glUniformMatrix4fv(glGetUniformLocation(program, "shadow_view_projection"), 1, GL_FALSE,
 				                   glm::value_ptr(light_matrix));
+				glUniform1f(glGetUniformLocation(program, "time"), static_cast<float>(nowTime) / 1000.0f );
 				glUniform3fv(glGetUniformLocation(program, "light_color"), 1, glm::value_ptr(lightColors[i]));
 				glUniform3fv(glGetUniformLocation(program, "light_position"), 1, glm::value_ptr(lightTransform.GetTranslation()));
 				glUniform3fv(glGetUniformLocation(program, "light_direction"), 1, glm::value_ptr(lightTransform.GetFront()));
@@ -404,18 +485,61 @@ edan35::Assignment2::run()
 		glUseProgram(0u);
 
 
+
 		//
 		// Pass 4: Draw wireframe cones on top of the final image for debugging purposes
 		//
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		for (size_t i = 0; i < constant::lights_nb; ++i) {
-			cone.render(mCamera.GetWorldToClipMatrix(),
-			            lightTransforms[i].GetMatrix() * lightOffsetTransform.GetMatrix() * coneScaleTransform.GetMatrix(),
-			            fill_shadowmap_shader, set_uniforms);
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		//for (size_t i = 0; i < constant::lights_nb; ++i) {
+		//	cone.render(mCamera.GetWorldToClipMatrix(),
+		//	            lightTransforms[i].GetMatrix() * lightOffsetTransform.GetMatrix() * coneScaleTransform.GetMatrix(),
+		//	            fill_shadowmap_shader, set_uniforms);
+		//}
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+		//
+		// Pass 5: Draw transparent objects
+		//
+		
+		glDepthFunc(GL_ALWAYS);
+		glEnable(GL_BLEND);
+		glDisable(GL_DEPTH_WRITEMASK);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		camera_position = mCamera.mWorld.GetTranslation();
+		for (int i = 0; i < constant::MaxSmokes / 3; i++) {
+			smokes[i].x = x_offset+10*sin((1000 * static_cast<float>(i) + nowTime) / 1500) + 10*pow(sin((1000 * static_cast<float>(i) + nowTime) / 1900), 2);
+			smokes[i].y = y_offset + fmod((10000 * static_cast<float>(i) + 10*nowTime) / 1000, 100);
+			smokes[i].z = z_offset + 10*sin((1000 * static_cast<float>(i) + nowTime) / 2000) + 10*pow(sin(nowTime / 1000), 2);
 		}
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		for (int i = constant::MaxSmokes / 3; i < 2 * constant::MaxSmokes / 3; i++) {
+			smokes[i].x = x_offset+ -10 - 10*sin((1000 * static_cast<float>(i) + nowTime) / 1750) + 10*pow(sin((1000 * static_cast<float>(i - 10) + nowTime) / 1700), 2);
+			smokes[i].y = y_offset + 10 + fmod((10000 * static_cast<float>(i) + 10*nowTime) / 1000, 100);
+			smokes[i].z = z_offset + 10*sin((1000 * static_cast<float>(i) + nowTime) / 1337) + 10*pow(sin((1000 * static_cast<float>(i) + nowTime) / 1200), 3);
+		}
+		for (int i = 2 * constant::MaxSmokes / 3; i < constant::MaxSmokes; i++) {
+			smokes[i].x =x_offset + -15 + sin((1000 * static_cast<float>(i) + nowTime) / 1337) + pow(sin((1000 * static_cast<float>(i - 20) + nowTime) / 1200), 3);
+			smokes[i].y =y_offset +  20 + fmod((10000 * static_cast<float>(i) + 10*nowTime) / 1000, 100);
+			smokes[i].z =z_offset +  -10 - 10*sin((1000 * static_cast<float>(i) + nowTime) / 1337) - 10*pow(sin((1000 * static_cast<float>(i - 20) + nowTime) / 2000), 5);
+		}
+		
 
-
+		
+		for (int i = 0; i < constant::MaxFires; i++) {
+			bind_texture_with_sampler(GL_TEXTURE_2D, 1, fire_shader, "depth_texture", depth_texture, depth_sampler);
+			glm::vec3 q_position = fires[i];
+			quad[i].render(mCamera.GetWorldToClipMatrix(), quad[i].get_transform(), mCamera.GetWorldToViewMatrix(), mCamera.GetViewToClipMatrix(), q_position);
+		}
+		bind_texture_with_sampler(GL_TEXTURE_2D, 0, smoke_shader, "depth_texture", depth_texture, depth_sampler);
+		for (int i = 0; i < constant::MaxSmokes; i++) {
+			smokeQuad[i].set_translation(smokes[i]);
+			number = fmod((1000 * static_cast<float>(i) + nowTime) / 1000, 10);
+			glm::vec3 q_position = smokes[i];
+			smokeQuad[i].render(mCamera.GetWorldToClipMatrix(), smokeQuad[i].get_transform(), mCamera.GetWorldToViewMatrix(), mCamera.GetViewToClipMatrix(), q_position);
+		}
+		glEnable(GL_DEPTH_TEST);
+		glDisable(GL_BLEND);
+		glDepthFunc(GL_ALWAYS);
+		glEnable(GL_DEPTH_WRITEMASK);
 		//
 		// Output content of the g-buffer as well as of the shadowmap, for debugging purposes
 		//
@@ -455,6 +579,10 @@ edan35::Assignment2::run()
 	fill_gbuffer_shader = 0u;
 	glDeleteProgram(fallback_shader);
 	fallback_shader = 0u;
+	glDeleteProgram(fire_shader);
+	fire_shader = 0u;
+	glDeleteProgram(smoke_shader);
+	smoke_shader = 0u;
 }
 
 int main()
